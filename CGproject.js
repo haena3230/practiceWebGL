8abd4e2423445cfbb7dba168c33e13cd0540f7be
haena3230 <gosk0912@naver.com>
2020-06-18 00:32:57 +0900

    itembox rendering

--- CGproject.js
+++ CGproject.js
@@ -1,5 +1,5 @@
 function init() {
-  // 위치 변화 변수
+  // 다리 움직임을 위한 변수
   var step = 0;
 
   // scene 생성
@@ -9,17 +9,40 @@ function init() {
   var camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+    1,
+    10000
   );
 
-  camera.position.set(40, 50, 65);
+  camera.position.set(50, 60, 100);
   camera.lookAt(scene.position);
 
   // renderer 생성
   var renderer = new THREE.WebGLRenderer();
-  renderer.setClearColor(new THREE.Color(0x000000));
+  renderer.setClearColor(new THREE.Color("rgb(204,204,204)"));
   renderer.setSize(window.innerWidth, window.innerHeight);
+
+  function createLights() {
+    light = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.5);
+
+    shadowLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    shadowLight.position.set(100, 200, 50);
+    shadowLight.castShadow = true;
+    shadowLight.shadowDarkness = 0.2;
+
+    backLight = new THREE.DirectionalLight(0xffffff, 0.4);
+    backLight.position.set(-100, 200, 50);
+    backLight.shadowDarkness = 0.1;
+    backLight.castShadow = true;
+
+    scene.add(backLight);
+    scene.add(light);
+    scene.add(shadowLight);
+  }
+
+  //색상 생성
+  var basecolor = new THREE.Color("rgb(255,204,153)");
+  var secondcolor = new THREE.Color("rgb(204, 153, 102)");
+  var thirdcolor = new THREE.Color("rgb(051, 000, 000)");
 
   // 쉐도우 맵 지정
   renderer.shadowMap.enabled = true;
@@ -29,9 +52,9 @@ function init() {
   axes.position.set(0, 0, 0);
 
   // 바닥판 생성
-  var planeGeometry = new THREE.PlaneGeometry(100, 60);
+  var planeGeometry = new THREE.PlaneGeometry(100, 200);
   var planeMaterial = new THREE.MeshLambertMaterial({
-    color: 0xffffff,
+    color: "rgb(000, 204, 000)",
   });
   var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
@@ -44,7 +67,9 @@ function init() {
 
   // 머리 생성
   var cubeGeometry = new THREE.CubeGeometry(7, 7, 7);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: basecolor,
+  });
   var head = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
   // 머리 위치 지정
@@ -53,103 +78,245 @@ function init() {
   //그림자 생성 오브젝트
   head.castShadow = true;
 
+  // 주둥이 생성
+  var cubeGeometry = new THREE.CubeGeometry(7, 3, 1);
+  cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var mouth = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  mouth.position.set(0, 17.5, 10.5);
+  mouth.castShadow = true;
+
   // 왼쪽 귀 생성
-  var cylinderGeometry = new THREE.CylinderBufferGeometry(1, 1, 2, 32);
-  var cylinderMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
-  var Lear = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+  var cubeGeometry = new THREE.CubeGeometry(1.5, 1.5, 1.5);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var Lear = new THREE.Mesh(cubeGeometry, cubeMaterial);
   Lear.position.set(-2, 24.5, 10);
   Lear.castShadow = true;
 
   // 오른쪽 귀 생성
-  var cylinderGeometry = new THREE.CylinderBufferGeometry(1, 1, 2, 32);
-  var cylinderMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
-  var Rear = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-  Rear.position.set(3, 24.5, 10);
+  var cubeGeometry = new THREE.CubeGeometry(1.5, 1.5, 1.5);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var Rear = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  Rear.position.set(2, 24.5, 10);
   Rear.castShadow = true;
 
+  //코 생성
+  var cubeGeometry = new THREE.CubeGeometry(1.5, 1, 1);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: thirdcolor,
+  });
+  var nose = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  nose.position.set(0, 18.5, 11.5);
+  nose.castShadow = true;
+
   // 왼쪽 눈 생성
-  var sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-  var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var sphereGeometry = new THREE.SphereGeometry(0.5, 20, 20);
+  var sphereMaterial = new THREE.MeshLambertMaterial({
+    color: thirdcolor,
+  });
   var Leye = new THREE.Mesh(sphereGeometry, sphereMaterial);
   Leye.position.set(-1.5, 21, 10.5);
   Leye.castShadow = true;
 
   // 오른쪽 눈 생성
-  var sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-  var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var sphereGeometry = new THREE.SphereGeometry(0.5, 20, 20);
   var Reye = new THREE.Mesh(sphereGeometry, sphereMaterial);
   Reye.position.set(1.5, 21, 10.5);
   Reye.castShadow = true;
 
+  // 머리 그룹
+  var headgroup = new THREE.Group();
+  headgroup.add(head);
+  headgroup.add(mouth);
+  headgroup.add(nose);
+  headgroup.add(Lear);
+  headgroup.add(Rear);
+  headgroup.add(Leye);
+  headgroup.add(Reye);
+
   // 목 생성
   var cubeGeometry = new THREE.CubeGeometry(2, 3, 2);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: basecolor,
+  });
   var neck = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  neck.position.set(0, 14.5, 5.5);
+  neck.position.set(0, 14.5, 6);
   neck.castShadow = true;
 
   // 몸통 생성
-  var cubeGeometry = new THREE.CubeGeometry(10, 10, 15);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeGeometry = new THREE.CubeGeometry(10, 8, 15);
   var body = new THREE.Mesh(cubeGeometry, cubeMaterial);
   body.position.set(0, 8, 0);
   body.castShadow = true;
 
   // 왼쪽 앞 다리 생성
-  var cubeGeometry = new THREE.CubeGeometry(3, 10, 3);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeGeometry = new THREE.CubeGeometry(2.5, 10, 2.5);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
   var LFleg = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  LFleg.position.set(-6.5, 5, 6);
+  LFleg.position.set(-7.5, 5, 6);
   LFleg.castShadow = true;
+
   // 오른쪽 앞 다리 생성
-  var cubeGeometry = new THREE.CubeGeometry(3, 10, 3);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeGeometry = new THREE.CubeGeometry(2.5, 10, 2.5);
   var RFleg = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  RFleg.position.set(6.5, 5, 6);
+  RFleg.position.set(7.5, 5, 6);
   RFleg.castShadow = true;
+
   // 왼쪽 뒷다리 생성
-  var cubeGeometry = new THREE.CubeGeometry(3, 10, 3);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeGeometry = new THREE.CubeGeometry(2.5, 10, 2.5);
   var LBleg = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  LBleg.position.set(-6.5, 5, -6);
+  LBleg.position.set(-7.5, 5, -6);
   LBleg.castShadow = true;
+
   // 오른쪽 뒷다리 생성
-  var cubeGeometry = new THREE.CubeGeometry(3, 10, 3);
-  var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+  var cubeGeometry = new THREE.CubeGeometry(2.5, 10, 2.5);
   var RBleg = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  RBleg.position.set(6.5, 5, -6);
+  RBleg.position.set(7.5, 5, -6);
   RBleg.castShadow = true;
-  // 조명 생성
-  var spotLight = new THREE.SpotLight(0xffffff);
-  spotLight.position.set(20, 80, 100);
-  spotLight.castShadow = true;
-  spotLight.shadow.mapSize = new THREE.Vector2(5120, 5120);
 
-  // 속도 조절 control
-  var controls = new (function () {
-    this.rotationSpeed = 0.02;
-    this.bouncingSpeed = 0.03;
-  })();
+  // 꼬리 생성
+  var cubeGeometry = new THREE.CubeGeometry(3, 3, 3);
+  var tail = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  tail.position.set(0, 11.5, -10);
+  tail.castShadow = true;
 
-  var gui = new dat.GUI();
-  gui.add(controls, "rotationSpeed", 0, 0.5);
-  gui.add(controls, "bouncingSpeed", 0, 0.5);
+  //몸통그룹
+  var bodygroup = new THREE.Group();
+  bodygroup.add(neck);
+  bodygroup.add(body);
+  bodygroup.add(LFleg);
+  bodygroup.add(LBleg);
+  bodygroup.add(RFleg);
+  bodygroup.add(RBleg);
+  bodygroup.add(tail);
+
+  // 캐릭터 그룹
+  var doggroup = new THREE.Group();
+  doggroup.add(headgroup);
+  doggroup.add(bodygroup);
+
+  // 선물BOX
+
+  // 왼쪽 위 itembox 생성
+  var cubeGeometry = new THREE.CubeGeometry(8, 8, 2);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: thirdcolor,
+  });
+  var LTone = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LTone.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(2, 8, 8);
+  var LTtwo = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LTtwo.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(7.9, 7.9, 7.9);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var LTbox = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LTbox.castShadow = true;
+
+  // 왼쪽 위itembox 그룹화
+  var LTitembox = new THREE.Group();
+  LTitembox.add(LTone);
+  LTitembox.add(LTtwo);
+  LTitembox.add(LTbox);
+  LTitembox.position.set(-40, 40, 0);
+
+  // 오른쪽 위 itembox 생성
+  var cubeGeometry = new THREE.CubeGeometry(8, 8, 2);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: thirdcolor,
+  });
+  var RTone = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  RTone.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(2, 8, 8);
+  var RTtwo = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  RTtwo.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(7.9, 7.9, 7.9);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var RTbox = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  RTbox.castShadow = true;
+
+  // 오른쪽 위itembox 그룹화
+  var RTitembox = new THREE.Group();
+  RTitembox.add(RTone);
+  RTitembox.add(RTtwo);
+  RTitembox.add(RTbox);
+  RTitembox.position.set(40, 40, 0);
+
+  // 왼쪽 밑 itembox 생성
+  var cubeGeometry = new THREE.CubeGeometry(8, 8, 2);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: thirdcolor,
+  });
+  var LBone = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LBone.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(2, 8, 8);
+  var LBtwo = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LBtwo.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(7.9, 7.9, 7.9);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var LBbox = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LBbox.castShadow = true;
+
+  // 왼쪽 밑 itembox 그룹화
+  var LBitembox = new THREE.Group();
+  LBitembox.add(LBone);
+  LBitembox.add(LBtwo);
+  LBitembox.add(LBbox);
+  LBitembox.position.set(-40, 10, 0);
+
+  // 오른쪽 밑 itembox 생성
+  var cubeGeometry = new THREE.CubeGeometry(8, 8, 2);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: thirdcolor,
+  });
+  var RBone = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LBone.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(2, 8, 8);
+  var RBtwo = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  LBtwo.castShadow = true;
+
+  var cubeGeometry = new THREE.CubeGeometry(7.9, 7.9, 7.9);
+  var cubeMaterial = new THREE.MeshLambertMaterial({
+    color: secondcolor,
+  });
+  var RBbox = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  RBbox.castShadow = true;
+
+  // 오른쪽 밑 itembox 그룹화
+  var RBitembox = new THREE.Group();
+  RBitembox.add(RBone);
+  RBitembox.add(RBtwo);
+  RBitembox.add(RBbox);
+  RBitembox.position.set(40, 10, 0);
 
   //scene에 객체들 추가
+  createLights();
   scene.add(axes);
-  scene.add(head);
-  scene.add(Lear);
-  scene.add(Rear);
-  scene.add(Leye);
-  scene.add(Reye);
-  scene.add(neck);
-  scene.add(body);
-  scene.add(LFleg);
-  scene.add(RFleg);
-  scene.add(LBleg);
-  scene.add(RBleg);
+  scene.add(doggroup);
   scene.add(plane);
-  scene.add(spotLight);
+  scene.add(LTitembox);
+  scene.add(RTitembox);
+  scene.add(LBitembox);
+  scene.add(RBitembox);
 
   // 변수 전달
   document.getElementById("threejs_scene").appendChild(renderer.domElement);
@@ -159,18 +326,28 @@ function init() {
 
   // 렌더링 함수
   function renderScene() {
+    //다리 움직임
+    RFleg.rotation.x += 0.02 * Math.cos(step);
+    step += 0.03;
+
+    LFleg.rotation.x += -0.02 * Math.cos(step);
+    step += 0.03;
+
+    RBleg.rotation.x += -0.02 * Math.cos(step);
+    step += 0.03;
+
+    LBleg.rotation.x += 0.02 * Math.cos(step);
+    step += 0.03;
+
+    // headgroup.rotation.y += 0.02 * Math.cos(step);
+    // step += 0.03;
+
+    // bodygroup.rotation.y += 0.02 * Math.cos(step);
+    // step += 0.03;
+
+    // 렌더링
     renderer.render(scene, camera);
-
     requestAnimationFrame(renderScene);
-    //큐브 회전
-    // cube.rotation.x += controls.rotationSpeed;
-    // cube.rotation.y += controls.rotationSpeed;
-    // cube.rotation.z += controls.rotationSpeed;
-
-    // 튀기는 큐브position 변경
-    // step += controls.bouncingSpeed;
-    // cube.position.x = 10 * Math.cos(step);
-    // cube.position.y = 10 + 10 * Math.abs(Math.sin(step));
   }
 }
 window.onload = init();
